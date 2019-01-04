ee67b878fde0dcd3ff9c2989f5a1c3188a550a90
talitalira <lira.talita@hotmail.com>
2019-01-04 16:35:06 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-with-react-fullstack-web-development
+This repo contains an application developed using React + Express/Node API + MongoDB
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+
+passport.use(new GoogleStrategy(
+  {
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback' 
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
+  }
+));
+// authorize http://localhost:5000/auth/google/callback on googleAPI's
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+// 'google' is an internal identifier for the instance GoogleStrategy declared previously
+
+app.get(
+  '/auth/google/callback', 
+  passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
