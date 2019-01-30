15f2fa66bdb407c07ef054a6d607658b065b8e3e
Talita Lira <lira.talita@hotmail.com>
2019-01-30 16:49:14 -0600

    fixing typing

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if(proccess.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
     // faz a requisição do objeto que contem as chaves de dev, e exporta ele imediatamente.
